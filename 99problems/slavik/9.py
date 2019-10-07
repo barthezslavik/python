daa859e0229149d071b419f48c00dc9ef0cdd47d
Loginov Vyacheslav <barthez.slavik@gmail.com>
2019-10-08 00:10:17 +0300

    9

--- 99problems/slavik/9.py
+++ 99problems/slavik/9.py
@@ -1,9 +1,11 @@
 def pack(list):
-    result = [];
-    for item in list:
-        if len(result) == 0 or result[-1][0] != item:
-            result.append(item) 
-        #result[-1].append(item)
-    return result
+	result = []
 
-print(pack([1, 2, 2, 3, 3, 4, 5]))
+	for index, item in enumerate(list):
+		if index == 0 or item != list[index-1]:
+			result.append([item])
+		else:
+			result[-1].append(item)
+	return result
+
+print(pack([1, 2, 2, 3, 3, 4, 5, 5, 2, 2, 1]))
