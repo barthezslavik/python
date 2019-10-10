c2c5377a7b480204ac208249454ac57be41257e7
Loginov Vyacheslav <barthez.slavik@gmail.com>
2019-10-10 22:01:00 +0300

    11

---
+++ 99problems/slavik/11.py
@@ -0,0 +1,21 @@
+def pack(list):
+    result = []
+
+    for index, item in enumerate(list):
+        if index == 0 or item != list[index-1]:
+            result.append([item])
+        else:
+            result[-1].append(item)
+    return result
+
+def encode(list):
+    acc = []
+    for item in list:
+        if len(item) == 1:
+            acc.append(item[0])
+        else:
+            element = [len(item), item[0]]
+            acc.append(element)
+    return acc
+
+print(encode(pack([1, 2, 2, 3, 3, 4, 5, 5, 2, 2, 1])))
